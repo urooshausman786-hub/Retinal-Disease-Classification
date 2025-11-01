be3d658ef0cebc3665297588f94ecfa9ce3ffcd1
urooshausman786-hub <urooshausman786@gmail.com>
2025-11-01 18:42:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import numpy as np
 from PIL import Image
-import tflite_runtime.interpreter as tflite
+import tensorflow as tf
+tflite = tf.lite
 
 st.set_page_config(page_title="Retinal Disease Classification", page_icon="👁️", layout="wide")
 
